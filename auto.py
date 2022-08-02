c484dac3dc1ae83ea235b1e6c1511a104d75a06e
jonas <>
2022-08-02 23:26:35 +0200

    old project moved to git

---
+++ auto.py
@@ -0,0 +1,25 @@
+from pynput.keyboard import Key, Controller
+import time
+
+string = "magicl2Love "
+
+keyboard = Controller()
+while True:
+    amount = int(input("size of triangle ")) + 1
+    print("attends 3 secondes")
+    time.sleep(3)
+    # keyboard.press(Key.ctrl)
+    for i in range(amount):
+        time.sleep(0.5)
+        for a in range(i):
+            keyboard.type(string)
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+    for i in reversed(range(amount - 1)):
+        time.sleep(0.5)
+        for a in range(i):
+            keyboard.type(string)
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+    # keyboard.release(Key.ctrl)
+    print("finished")
